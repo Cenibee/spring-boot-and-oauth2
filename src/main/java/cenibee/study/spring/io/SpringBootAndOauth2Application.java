865cf9492723c01fbf7dd07ca49e2e91d914a3be
Cenibee <30590761+Cenibee@users.noreply.github.com>
2021-03-10 11:54:40 +0900

    :tada: 프로젝트 생성

---
+++ src/main/java/cenibee/study/spring/io/SpringBootAndOauth2Application.java
@@ -0,0 +1,13 @@
+package cenibee.study.spring.io;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAndOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAndOauth2Application.class, args);
+	}
+
+}
